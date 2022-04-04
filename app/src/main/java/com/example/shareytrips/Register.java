e6867ec77d815e6593537db602151dc574fef267
Aggelos <aggelospsiris3@gmail.com>
2022-04-05 00:13:10 +0300

    version1

--- app/src/main/java/com/example/shareytrips/Register.java
+++ app/src/main/java/com/example/shareytrips/Register.java
@@ -1,8 +1,11 @@
 package com.example.shareytrips;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,17 +14,35 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Register extends AppCompatActivity {
     private EditText mFullName,mEmail,mPassword,mConfPassword;
     private Button mRegisterBtn;
     private TextView mLoginBtn;
 
-    FirebaseAuth fAuth;
+    //for interests checkbox items
+    private Button interestsBtn;
+    private TextView interestsSelected;
+    private String[] listItems;
+    private boolean[] checkedItems;
+    private ArrayList<Integer> UserInterests = new ArrayList<>();
+
+    private FirebaseAuth fAuth;
+    private DatabaseReference mRootRef;
+
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +56,82 @@ public class Register extends AppCompatActivity {
         mRegisterBtn= findViewById(R.id.registerBtn);
         mLoginBtn   = findViewById(R.id.createText);
 
+        mRootRef = FirebaseDatabase.getInstance().getReference();
         fAuth = FirebaseAuth.getInstance();
 
+        pd = new ProgressDialog(this);
+
+        /**********************************************************************************************************************/
+        //for interests checkbox items
+        interestsBtn = (Button) findViewById(R.id.interestsBtn);
+        interestsSelected = (TextView) findViewById(R.id.interestsText);
+        listItems = getResources().getStringArray(R.array.interests);
+        checkedItems = new boolean[listItems.length];
+
+        interestsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(Register.this);
+                mBuilder.setTitle("Travelling interests");
+                mBuilder.setMultiChoiceItems(listItems, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int position, boolean isChecked) {
+//                       if (isChecked) {
+//                            if (!mUserItems.contains(position)) {
+//                                mUserItems.add(position);
+//                            }
+//                        } else if (mUserItems.contains(position)) {
+//                            mUserItems.remove(position);
+//                        }
+                        if(isChecked){
+                            UserInterests.add(position);
+                        }else{
+                            UserInterests.remove((Integer.valueOf(position)));
+                        }
+                    }
+                });
+
+                mBuilder.setCancelable(false);
+                mBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        String item = "";
+                        for (int i = 0; i < UserInterests.size(); i++) {
+                            item = item + listItems[UserInterests.get(i)];
+                            if (i != UserInterests.size() - 1) {
+                                item = item + ", ";
+                            }
+                        }
+                        interestsSelected.setText(item);
+                    }
+                });
+
+                mBuilder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                mBuilder.setNeutralButton("Clear all", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        for (int i = 0; i < checkedItems.length; i++) {
+                            checkedItems[i] = false;
+                            UserInterests.clear();
+                            interestsSelected.setText("");
+                        }
+                    }
+                });
+
+                AlertDialog mDialog = mBuilder.create();
+                mDialog.show();
+            }
+        });
+        /*Interests checkbox end*/
+        /**********************************************************************************************************************/
+
+        //change to login page
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,13 +139,7 @@ public class Register extends AppCompatActivity {
                 finish();
             }
         });
-        mLoginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),Login.class));
-            }
-        });
-
+        //register
         mRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -84,14 +173,23 @@ public class Register extends AppCompatActivity {
                     return;
                 }
 
+                pd.setMessage("Please wait");
+                pd.show();
                 //data is validated
                 //register the user using firebase
-                Toast.makeText(Register.this,"Data validated",Toast.LENGTH_SHORT).show();
 
                 fAuth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                     @Override
                     public void onSuccess(AuthResult authResult) {
-                        //send user to next page
+                        //create users data tree
+                        HashMap<String,Object> map = new HashMap<>();
+                        map.put("fullname",fullName);
+                        map.put("email",email);
+                        map.put("id",fAuth.getCurrentUser().getUid());
+                        map.put("interests",interestsSelected.getText().toString());
+                        mRootRef.child("Users").child(fAuth.getCurrentUser().getUid()).setValue(map);
+                        //send user to main page
+                        pd.dismiss();
                         startActivity(new Intent(getApplicationContext(),MainActivity.class));
                         finish();
                     }
