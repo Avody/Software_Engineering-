110076f4efa10eb6d971c27fb4db08fd5bd83a04
Aggelos <aggelospsiris3@gmail.com>
2022-06-11 15:33:49 +0300

    profile fragment on built

--- app/src/main/java/com/example/shareytrips/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/shareytrips/Fragments/ProfileFragment.java
@@ -1,22 +1,106 @@
 package com.example.shareytrips.Fragments;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.shareytrips.R;
+import com.example.shareytrips.Register;
+
+import java.util.ArrayList;
 
 
 public class ProfileFragment extends Fragment {
 
+    //for interests checkbox items
+    private Button interestsBtn;
+    private TextView interestsSelected;
+    private String[] listItems;
+    private boolean[] checkedItems;
+    private ArrayList<Integer> UserInterests = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //for interests checkbox items
+        interestsBtn = (Button) getView().findViewById(R.id.updateInterests);
+        interestsSelected = (TextView) getView().findViewById(R.id.updateText);
+        listItems = getResources().getStringArray(R.array.interests);
+        checkedItems = new boolean[listItems.length];
+
+        interestsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
+                mBuilder.setTitle("Travelling interests");
+                mBuilder.setMultiChoiceItems(listItems, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int position, boolean isChecked) {
+//                       if (isChecked) {
+//                            if (!mUserItems.contains(position)) {
+//                                mUserItems.add(position);
+//                            }
+//                        } else if (mUserItems.contains(position)) {
+//                            mUserItems.remove(position);
+//                        }
+                        if(isChecked){
+                            UserInterests.add(position);
+                        }else{
+                            UserInterests.remove((Integer.valueOf(position)));
+                        }
+                    }
+                });
+
+                mBuilder.setCancelable(false);
+                mBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        String item = "";
+                        for (int i = 0; i < UserInterests.size(); i++) {
+                            item = item + listItems[UserInterests.get(i)];
+                            if (i != UserInterests.size() - 1) {
+                                item = item + ", ";
+                            }
+                        }
+                        interestsSelected.setText(item);
+                    }
+                });
+
+                mBuilder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                mBuilder.setNeutralButton("Clear all", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        for (int i = 0; i < checkedItems.length; i++) {
+                            checkedItems[i] = false;
+                            UserInterests.clear();
+                            interestsSelected.setText("");
+                        }
+                    }
+                });
+
+                AlertDialog mDialog = mBuilder.create();
+                mDialog.show();
+            }
+        });
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
